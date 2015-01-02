fc889a8ebe3d171870e89c890f307055ae9042fa
Piotr Majcherczyk <fynxor@gmail.com>
2015-01-02 20:27:33 +0100

    Add header Strings.hpp itself.

---
+++ src/Common/Strings.hpp
@@ -0,0 +1,22 @@
+/* YoLoDevelopment, 2014
+ * All rights reserved.
+ */
+#pragma once
+#include <QtCore>
+
+namespace BS {
+	namespace Strings {
+
+		namespace Animators {
+			const QString Animator      = "AnimatorAnimator";
+			const QString ApplyDamage   = "AnimatorApplyDamage";
+			const QString Consume       = "AnimatorConsume";
+			const QString Die           = "AnimatorDie";
+			const QString Move          = "AnimatorMove";
+			const QString PhysicsUpdate = "AnimatorPhysicsUpdate";
+			const QString Regenerate    = "AnimatorRegenerate";
+			const QString Test          = "AnimatorTest";
+		}
+
+	}
+}
